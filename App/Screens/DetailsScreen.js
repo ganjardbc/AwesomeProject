22ed3d63a864125ef50a87fefbb9724cdc6b1c33
Ganjar Hadiatna <ganjardbc@gmail.com>
2021-03-26 11:03:56 +0700

    Implementing props data

--- App/Screens/DetailsScreen.js
+++ App/Screens/DetailsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button, FlatList } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 class DetailsScreen extends Component {
     constructor() {
@@ -14,20 +14,13 @@ class DetailsScreen extends Component {
 
         return (
             <View>
-                <FlatList 
-                    data={data}
-                    renderItem={({item}) => {
-                        return (
-                            <View>
-                                <Text>{ item.title }</Text>
-                                <Text>{ item.description }</Text>
-                            </View>
-                        )
-                    }}
-                />
-                {/* <Button title="Go to Details... again" onPress={() => navigation.push('Details')} />
+                <View>
+                    <Text>{ data && data.title }</Text>
+                    <Text>{ data && data.description }</Text>
+                </View>
+                <Button title="Go to Details... again" onPress={() => navigation.push('Details')} />
                 <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-                <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+                <Button title="Go back" onPress={() => navigation.goBack()} />
             </View>
         )
     }
--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, FlatList } from 'react-native'
 
 class HomeScreen extends Component {
     constructor() {
@@ -14,9 +14,23 @@ class HomeScreen extends Component {
             {id: 2, title: 'TEST LAGI', description: 'TEST LAGI'}
         ]
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ fontSize: 32, marginBottom: 15 }}>Home Screen</Text>
-                <Button title="Go to Details" onPress={() => navigation.navigate('Details', payload)} />
+            <View>
+                <FlatList 
+                    data={payload}
+                    renderItem={({item}) => {
+                        return (
+                            <View>
+                                <View>
+                                    <Text>{ item.title }</Text>
+                                    <Text>{ item.description }</Text>
+                                </View>
+                                <View>
+                                    <Button title="Go to Details" onPress={() => navigation.navigate('Details', item)} />
+                                </View>
+                            </View>
+                        )
+                    }}
+                />
             </View>
         )
     }
